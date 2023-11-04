3b12e801dbe5c0d6ffdf91017e3b6ce3b3800215
Nick Moreton <nick.moreton@torchbox.com>
2023-11-04 13:40:28 +0000

    Fix django warning about default_auto_field

--- wagtail_devtools/test/apps.py
+++ wagtail_devtools/test/apps.py
@@ -5,3 +5,4 @@ class WagtailDevtoolsTestAppConfig(AppConfig):
     label = "wagtail_devtools_test"
     name = "wagtail_devtools.test"
     verbose_name = "Wagtail devtools tests"
+    default_auto_field = "django.db.models.AutoField"
