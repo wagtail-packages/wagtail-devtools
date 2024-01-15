0021732de256ea917a22a6925f0341d173a0c1eb
Nick Moreton <nickmoreton@me.com>
2024-01-15 09:38:46 +0000

    Remove code comments

--- wagtail_devtools/test/tests/test_views.py
+++ wagtail_devtools/test/tests/test_views.py
@@ -1,27 +1,8 @@
 from django.test import TestCase
 
 
-# from wagtail_devtools.test.management.commands.build_fixtures import (
-#     create_form_pages,
-# )
-
-# from wagtail_devtools.api.views import (
-#     api_view,
-#     form_types,
-#     model_admin_types,
-#     page_model_types,
-#     settings_types,
-#     snippet_types,
-#     wagtail_core_edit_pages,
-#     wagtail_core_listing_pages,
-# )
-
-
 class TestApiViews(TestCase):
-    # fixtures = ["wagtail_devtools/test/fixtures/fixtures.json"]
-
     def test_api_view(self):
-        # liver_server_url = self.live_server_url
         response = self.client.get("/wagtail-devtools-api/")
         self.assertEqual(response.status_code, 200)
 
@@ -55,3 +36,5 @@ class TestApiViews(TestCase):
             api_views[6],
             "http://localhost:8000/wagtail-devtools-api/snippet-types/",
         )
+
+    # TODO: Add further view tests. The views need to be altered to achieve this.
