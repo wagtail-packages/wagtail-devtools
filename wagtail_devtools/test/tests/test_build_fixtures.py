1372c6c289ac9ebfb3257f19b62e69fafa75a817
Nick Moreton <nickmoreton@me.com>
2024-01-15 09:35:03 +0000

    Optimise the test_build_fixtures test

--- wagtail_devtools/test/tests/test_build_fixtures.py
+++ wagtail_devtools/test/tests/test_build_fixtures.py
@@ -23,65 +23,48 @@ class TestBuildFixtures(TestCase):
     There is a custom test runner in test/runner (WagtailDevToolsTestRunner) that runs after the test suite has run
     that rebuilds the test-media folder."""
 
-    def test_fixtures(self):
-        with StringIO() as out:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=out)
-            self.assertIn("Fixtures created.", out.getvalue())
-
-    def test_fixtures_creates_pages(self):
+    @classmethod
+    def setUpTestData(cls):
+        media_path = Path(os.getcwd()) / "test-media"
+        if media_path.exists():
+            shutil.rmtree(media_path)
         with StringIO() as _:
             # Don't want to see the output of the command
             call_command("build_fixtures", "--clear", stdout=_)
+            cls.out = _.getvalue()
+
+    def test_fixtures(self):
+        self.assertIn("Fixtures created.", self.out)
+
+    def test_fixtures_creates_pages(self):
         pages = Page.objects.all().exclude(title="Root")
         self.assertEqual(pages.count(), 14)
 
     def test_fixtures_creates_snippets(self):
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
         snippet_models = get_snippet_models()
         for model in snippet_models:
             self.assertEqual(model.objects.count(), 4)
 
     def test_fixtures_creates_media(self):
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
         image_model = get_image_model()
         self.assertEqual(image_model.objects.count(), 4)
 
     def test_fixtures_creates_documents(self):
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
         document_model = get_document_model()
         self.assertEqual(document_model.objects.count(), 4)
 
     def test_fixtures_creates_user(self):
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
         user_model = get_user_model()
         self.assertEqual(user_model.objects.count(), 1)
 
     def test_fixtures_creates_collections(self):
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
         collection_model = Collection
         self.assertEqual(collection_model.objects.count(), 5)
 
     def test_fixtures_creates_settings(self):
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
         self.assertEqual(len(settings_registry), 6)
 
     def test_fixtures_creates_sites(self):
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
         site_model = Site
         self.assertEqual(site_model.objects.count(), 2)
 
@@ -98,27 +81,13 @@ class TestBuildFixtures(TestCase):
         self.assertEqual(second_site.root_page.title, "Second Site Home Page")
 
     def test_fixtures_creates_redirects(self):
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
         redirect_model = Redirect
         self.assertEqual(redirect_model.objects.count(), 4)
 
     def test_fixtures_creates_search_promotions(self):
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
         search_promotion_model = SearchPromotion
         self.assertEqual(search_promotion_model.objects.count(), 4)
 
     def test_fixtures_create_test_media(self):
         media_path = Path(os.getcwd()) / "test-media"
-        if media_path.exists():
-            shutil.rmtree(media_path)
-        self.assertFalse(media_path.exists())
-
-        with StringIO() as _:
-            # Don't want to see the output of the command
-            call_command("build_fixtures", "--clear", stdout=_)
-
         self.assertTrue(media_path.exists())
